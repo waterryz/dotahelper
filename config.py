2edc61c6db970adec32fdd0812c2848bd0d154b6
waterryz <alexyss.waterry@icloud.com>
2025-10-15 03:18:48 +0500

    Delete config.py

--- config.py
+++
@@ -1,5 +0,0 @@
-import os
-
-OPENAI_API_KEY = os.getenv("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")
-TELEGRAM_TOKEN = os.getenv("8291013351:AAE77aq5TTI8HN7HIRX-QuqeeaqpNEAK-D0")
-RENDER_EXTERNAL_HOSTNAME = os.getenv("dotatoxic.onrender.com")
